6ea8b447bca4a4f0e37c22623b0b2ccc9b786213
Mandytrajano90 <mandytrajano90@gmail.com>
2024-05-09 18:49:49 -0300

    Req 6 - feat protocol Report

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,13 @@
-class Report:
-    pass
+from abc import abstractmethod
+from typing import Protocol
+from inventory_report.inventory import Inventory
+
+
+class Report(Protocol):
+    @abstractmethod
+    def add_inventory(self, inventory: Inventory) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def generate(self) -> str:
+        raise NotImplementedError
