1c7c91aa20888d071f08e9496eae1b79346139a3
Mandytrajano90 <mandytrajano90@gmail.com>
2024-05-09 14:45:30 -0300

    Req 3, 4 and 8 - feat importers

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,16 +1,43 @@
-from typing import Dict, Type
+from typing import Dict, Type, List
+from abc import ABC, abstractmethod
+import json
+from csv import DictReader
+from inventory_report.product import Product
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]:
+        raise NotImplementedError
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+
+    def import_data(self) -> List[Product]:
+        with open(self.file_path) as file:
+            data = json.load(file)
+            products = []
+            for product in data:
+                products.append(Product(**product))
+            return products
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+
+    def import_data(self) -> List[Product]:
+        with open(self.file_path) as file:
+            data = DictReader(file)
+            products = []
+            for product in data:
+                products.append(Product(**product))
+            return products
 
 
 # Não altere a variável abaixo
