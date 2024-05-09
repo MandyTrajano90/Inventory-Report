bbfb2e68f3820695e616fd15c248ca444881527e
Mandytrajano90 <mandytrajano90@gmail.com>
2024-05-09 14:30:17 -0300

    Req 2 - test of product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,16 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="1",
+        product_name="Borracha",
+        company_name="Papelaria Solar",
+        manufacturing_date="2021-07-04",
+        expiration_date="2029-02-09",
+        serial_number="FR48",
+        storage_instructions="Ao abrigo de luz solar"
+    )
+    assert product.__str__() == (
+        f"The product {product.id} - {product.product_name} with serial number {product.serial_number} manufactured on {product.manufacturing_date} by the company {product.company_name} valid until {product.expiration_date} must be stored according to the following instructions: {product.storage_instructions}."
+    )
