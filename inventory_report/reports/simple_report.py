4c15189bd763958c3b22c3c467e83866462a6460
Mandytrajano90 <mandytrajano90@gmail.com>
2024-05-09 19:44:14 -0300

    Req 7 - feat SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,44 @@
-class SimpleReport:
-    pass
+from inventory_report.inventory import Inventory
+from inventory_report.reports.report import Report
+
+
+class SimpleReport(Report):
+    def __init__(self):
+        self.inventory_list = []
+
+    def add_inventory(self, inventory: Inventory):
+        self.inventory_list.append(inventory)
+
+    def _get_manufacturing_date(self) -> str:
+        return min(
+            product.manufacturing_date for inventory in self.inventory_list
+            for product in inventory.data
+            )
+
+    def _get_expiration_date(self) -> str:
+        return min(
+            product.expiration_date for inventory in self.inventory_list
+            for product in inventory.data
+            )
+
+    def _get_biggest_company(self) -> str:
+        companies = [
+            product.company_name for inventory in self.inventory_list
+            for product in inventory.data]
+        return max(companies, key=companies.count)
+
+    def generate(self) -> str:
+        if not self.inventory_list:
+            return "Inventory is empty"
+
+        oldest_manufact = self._get_manufacturing_date()
+        biggest_company = self._get_biggest_company()
+        expiration_date = self._get_expiration_date()
+
+        result = (
+            f"Oldest manufacturing date: {oldest_manufact}\n"
+            f"Closest expiration date: {expiration_date}\n"
+            f"Company with the largest inventory: {biggest_company}\n"
+        )
+
+        return result
