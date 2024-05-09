5f796b093ab86abb631bc3a9205b6a707b12a91f
Mandytrajano90 <mandytrajano90@gmail.com>
2024-05-09 14:13:21 -0300

    Req 1 - test of test_product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,3 +1,20 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
-#Starting
+    product = Product(
+        id="1",
+        product_name="Borracha",
+        company_name="Papelaria Solar",
+        manufacturing_date="2021-07-04",
+        expiration_date="2029-02-09",
+        serial_number="FR48",
+        storage_instructions="Ao abrigo de luz solar"
+    )
+    assert product.id == "1"
+    assert product.product_name == "Borracha"
+    assert product.company_name == "Papelaria Solar"
+    assert product.manufacturing_date == "2021-07-04"
+    assert product.expiration_date == "2029-02-09"
+    assert product.serial_number == "FR48"
+    assert product.storage_instructions == "Ao abrigo de luz solar"
